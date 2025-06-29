6e2af8a3ffe9fd84ab7c9383e4f0aac2f61c1cc9
sunrise-hackdays <sunrise.hackdays@gmail.com>
2025-06-30 05:48:08 +0900

    OGP設定を追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="React Dashboard Portfolio" />
+        <meta property="og:description" content="通知・統計を備えたUIポートフォリオ" />
+        <meta property="og:image" content="/ogp.png" />
+        <meta property="og:url" content="https://react-dashboard-hazel-zeta.vercel.app/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
